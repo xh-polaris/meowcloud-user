e2d16e5ed7ca57372ad457ece0ee462f3e4449f1
Mingle-2012 <1610365132@qq.com>
2024-08-12 01:21:59 +0800

    refactor: Remove unused code related to cat albums

--- biz/adaptor/server.go
+++ biz/adaptor/server.go
@@ -3,7 +3,6 @@ package adaptor
 import (
 	"context"
 
-	"github.com/xh-polaris/service-idl-gen-go/kitex_gen/meowcloud/content"
 	"github.com/xh-polaris/service-idl-gen-go/kitex_gen/meowcloud/user"
 
 	"github.com/xh-polaris/meowcloud-user/biz/application/service"
@@ -12,8 +11,7 @@ import (
 
 type UserServerImpl struct {
 	*config.Config
-	UserService     service.UserService
-	CatAlbumService service.CatAlbumService
+	UserService service.UserService
 }
 
 func (s *UserServerImpl) GetUser(ctx context.Context, req *user.GetUserReq) (res *user.GetUserResp, err error) {
@@ -26,24 +24,4 @@ func (s *UserServerImpl) GetUserDetail(ctx context.Context, req *user.GetUserDet
 
 func (s *UserServerImpl) UpdateUser(ctx context.Context, req *user.UpdateUserReq) (res *user.UpdateUserResp, err error) {
 	return s.UserService.UpdateUser(ctx, req)
-}
-
-func (s *UserServerImpl) CreateCatAlbum(ctx context.Context, req *content.CreateCatAlbumReq) (res *content.CreateCatAlbumResp, err error) {
-	return s.CatAlbumService.CreateCatAlbum(ctx, req)
-}
-
-func (s *UserServerImpl) RetrieveCatAlbum(ctx context.Context, req *content.RetrieveCatAlbumReq) (res *content.RetrieveCatAlbumResp, err error) {
-	return s.CatAlbumService.RetrieveCatAlbum(ctx, req)
-}
-
-func (s *UserServerImpl) UpdateCatAlbum(ctx context.Context, req *content.UpdateCatAlbumReq) (res *content.UpdateCatAlbumResp, err error) {
-	return s.CatAlbumService.UpdateCatAlbum(ctx, req)
-}
-
-func (s *UserServerImpl) DeleteCatAlbum(ctx context.Context, req *content.DeleteCatAlbumReq) (res *content.DeleteCatAlbumResp, err error) {
-	return s.CatAlbumService.DeleteCatAlbum(ctx, req)
-}
-
-func (s *UserServerImpl) ListCatAlbum(ctx context.Context, req *content.ListCatAlbumReq) (res *content.ListCatAlbumResp, err error) {
-	return s.CatAlbumService.ListCatAlbum(ctx, req)
 }
